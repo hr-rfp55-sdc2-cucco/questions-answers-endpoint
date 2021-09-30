dc01423d256d21c8d7372a08611c5b9c0f0540b8
Isabell Lee <izzy.isabell@gmail.com>
2021-09-30 13:59:14 -0700

    npm init and package install

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.end('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log('Listening on port: ', PORT);
+});
