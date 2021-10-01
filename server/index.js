c08e1dff84343f1030d780b3ba55ba87c25bb0de
Isabell Lee <izzy.isabell@gmail.com>
2021-10-01 11:18:11 -0700

    query answers for each question, needs formating and photos Please enter the commit message for your changes. Lines starting

--- server/index.js
+++ server/index.js
@@ -21,43 +21,38 @@ app.get('/qa/questions', (req, res) => {
   )
     .then((dataQ) => {
       let results = [];
-      dataQ.forEach((q) => {
-        let answers = [];
-        db.getAllAnswersByQuestionID(q.question_id)
-          .then((dataA) => {
-            dataA.forEach((a) => {
-              answers.push({
-                answer_id: a.answer_id,
-                body: a.body,
-                date: a.date,
-                answerer_name: a.answerer_name,
-                helpfulness: a.helpful,
-                photos: [],
-              })
-            })
-          })
-          .then(() => {
-            results.push({
+      let promiseQ = dataQ.map((question) => {
+        return db.getAllAnswersByQuestionID(question.question_id)
+          .then((answers) => answers.map((a) => {
+            return {
+              answer_id: a.id,
+              body: a.body,
+              date: a.date,
+              answerer_name: a.answerer_name,
+              helpfulness: a.helpful,
+              photos: [],
+            }
+          }))
+      })
+      Promise.all(promiseQ)
+        .then((promiseResults) => {
+          // console.log(promiseResults);
+          return dataQ.map((q, i) => {
+            return {
               question_id: q.question_id,
               question_body: q.question_body,
               question_date: q.question_date,
               asker_name: q.asker_name,
               question_helpfulness: q.helpful,
               reported: q.reported,
-              answers,
-            });
-          })
-          .catch((err) => console.error(err));
-      })
-      let returnObj = {
-        product_id: req.query.product_id,
-        results,
-      };
-      console.log('data from query', returnObj);
+              answers: promiseResults[i],
+            }
+          });
+        })
+        .then((result) => res.end(JSON.stringify(result)))
+        .catch((err) => console.error(err));
     })
     .catch((err) => console.error(err));
-  // console.log(req.query);
-  res.end();
 });
 
 app.listen(PORT, () => {
